1699ff2c33298c6fa3944964141f9bcb28a511e4
Groupmk <kruglovmikhail5@gmail.com>
2023-09-25 22:49:38 +0300

    git_api

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "Groupmk",
+  "avatar": "https://avatars.githubusercontent.com/u/120462306?v=4",
+  "name": "Mikhail Kruglov",
+  "company": "",
+  "blog": "",
+  "location": "Moscow",
+  "bio": "React developer",
+  "twitter": "",
+  "repos": 12,
+  "followers": 0,
+  "following": 0,
+  "created": "2022-12-13T10:48:45Z",
+}
